bc878c736175024d47752e223cbf385bf0123a3b
Neel1204 <neel9770635705@gmail.com>
2021-05-26 22:02:09 -0500

    quiz completed

---
+++ src/app/services/pouch.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { Show } from '../models/show.model';
+import * as PouchDB from 'pouchdb';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class PouchService {
+  constructor() {
+    // const db = new PouchDB('watchlist', {});
+  }
+
+  addShow(newShow: Show): void {}
+
+  getAllShows(): Array<Show> {
+    return null;
+  }
+
+}
+© 2021 GitHub, Inc.
